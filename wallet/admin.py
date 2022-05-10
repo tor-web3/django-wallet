eeeac6840614255497875d79c60bb5a2677a1a84
louie-home <727057301@qq.com>
2022-05-10 19:32:34 +0800

    时间字段抽象化

--- wallet/admin.py
+++ wallet/admin.py
@@ -5,24 +5,24 @@ from .models import *
 class PubkeyAdmin(admin.ModelAdmin):
     list_display = ("user","public_key","chain",)
     search_fields = ("public_key",)
-    ordering = ('-created_time',)
+    ordering = ('-created_at',)
     empty_value_display = '-'
     
-    def has_add_permission(self, request, *args, **kwargs):
-        return False
-    def has_change_permission(self, request, *args, **kwargs):
-        return False
+    # def has_add_permission(self, request, *args, **kwargs):
+    #     return False
+    # def has_change_permission(self, request, *args, **kwargs):
+    #     return False
 
 class ChainAdmin(admin.ModelAdmin):
-    list_display = ("chain_name","chain_network","chain_symbol","created_time","updated_time","explorer_url")
+    list_display = ("chain_name","chain_network","chain_symbol","created_at","updated_at","explorer_url")
     search_fields = ("chain_name", "chain_symbol")
-    ordering = ('-updated_time',)
+    ordering = ('-updated_at',)
     empty_value_display = '-'
 
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ("chain","contract_address","token_symbol","token_name","is_active","created_time","updated_time")
+    list_display = ("chain","contract_address","token_symbol","token_name","is_active","created_at","updated_at")
     search_fields = ("contract_address", "token_symbol")
-    ordering = ('-updated_time',)
+    ordering = ('-updated_at',)
     list_editable = ['is_active']
     empty_value_display = '-'
 
@@ -31,13 +31,13 @@ class AddressAdmin(admin.ModelAdmin):
     list_display = (
         "user","pubkey","index",
         'address','is_active',
-        'created_time',
+        'created_at',
     )
     search_fields = ('address',)
     #list_per_page设置每页显示多少条记录，默认是100条
     list_per_page = 25
     #ordering设置默认排序字段，负号表示降序排序
-    ordering = ('-created_time',)
+    ordering = ('-created_at',)
 
     # list_editable 设置默认可编辑字段
     list_editable = ['is_active']
@@ -47,10 +47,10 @@ class AddressAdmin(admin.ModelAdmin):
     # 字段为空时的默认显示
     empty_value_display = '-'
     
-    def has_add_permission(self, request, *args, **kwargs):
-        return False
-    def has_change_permission(self, request, *args, **kwargs):
-        return False
+    # def has_add_permission(self, request, *args, **kwargs):
+    #     return False
+    # def has_change_permission(self, request, *args, **kwargs):
+    #     return False
     
 admin.site.register(Address,AddressAdmin)
 admin.site.register(Chain,ChainAdmin)
