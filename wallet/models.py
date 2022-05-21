cf38d22cb1d7b230980c5c11cb6ec6434e1dcb40
nickname <410058486@qq.com>
2022-05-21 14:44:17 +0800

    nothing

--- wallet/models.py
+++ wallet/models.py
@@ -37,21 +37,6 @@ class Chain(CreateUpdateTracker):
         verbose_name = _('chain')
         verbose_name_plural = _('chain')
 
-class RPC(CreateUpdateTracker):
-    chain = models.ForeignKey(Chain, related_name='wallet_rpc', on_delete=models.CASCADE)
-    company = models.CharField(verbose_name=_("company"),max_length=128)
-    alias = models.CharField(verbose_name=_("alias"),max_length=128,blank=True,null=True,)
-    endpoint = models.CharField(verbose_name=_("endpoint"),max_length=128)
-    username = models.CharField(verbose_name=_("username"),max_length=128,blank=True,null=True,)
-    password = models.CharField(verbose_name=_("password"),max_length=128,blank=True,null=True,)
-    auth = models.CharField(verbose_name=_("auth"),max_length=128,choices=AUTH_TYPE,default=NORMAL)
-    
-    def __str__(self):
-        return f"{self.alias}-[{self.company}]"
-
-    class Meta:
-        verbose_name = _('RPC')
-        verbose_name_plural = _('RPC')
 
 class Pubkey(CreateUpdateTracker):
     user = models.ForeignKey(get_user_model(),related_name='wallet_pubkey',blank=True,null=True,verbose_name=_("User ID"),on_delete=models.CASCADE)
@@ -134,7 +119,30 @@ class Address(CreateUpdateTracker):
 
 
 
+class RPC(CreateUpdateTracker):
+    chain = models.ForeignKey(Chain, related_name='wallet_rpc', on_delete=models.CASCADE)
+    company = models.CharField(verbose_name=_("company"),max_length=128)
+    alias = models.CharField(verbose_name=_("alias"),max_length=128,blank=True,null=True,)
+    endpoint = models.CharField(verbose_name=_("endpoint"),max_length=128)
+    username = models.CharField(verbose_name=_("username"),max_length=128,blank=True,null=True,)
+    password = models.CharField(verbose_name=_("password"),max_length=128,blank=True,null=True,)
+    auth = models.CharField(verbose_name=_("auth"),max_length=128,choices=AUTH_TYPE,default=NORMAL)
+    
+    def __str__(self):
+        return f"{self.alias}-[{self.company}]"
+
+    class Meta:
+        verbose_name = _('RPC')
+        verbose_name_plural = _('RPC')
+
+
+class StateManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
 class State(CreateUpdateTracker):
+    objects = StateManager()
+
     address = models.ForeignKey(Address, related_name="wallet_state",on_delete=models.CASCADE)
     balance = models.JSONField(verbose_name=_('balance'))
     next_time = models.DateTimeField(
@@ -160,3 +168,7 @@ class State(CreateUpdateTracker):
 
     def __str__(self):
         return f"{self.is_update}"
+
+    class Meta:
+        verbose_name = _('State')
+        verbose_name_plural = _('Status')
