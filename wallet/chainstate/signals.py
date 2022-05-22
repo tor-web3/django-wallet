5e789472376238a514fa5cf21f25a29973f55f70
louie <727057301@qq.com>
2022-05-22 18:41:02 +0800

    新增地址更新信号

---
+++ wallet/chainstate/signals.py
@@ -0,0 +1,5 @@
+from django.dispatch import Signal
+
+
+# 地址更新: address
+wallet_address_updated = Signal()
---
+++ wallet/chainstate/tasks.py
@@ -0,0 +1,15 @@
+# from wallet.chainstate.signals import wallet_address_updated
+
+# from typing import Union, List, Optional, Dict
+
+# from celery.utils.log import get_task_logger
+
+# import requests
+
+# def check_eth_address_status(
+#     addresses: List[str],
+# ):
+#     requests.get()
+#     wallet_address_updated.send("check_address_status",address="")
+    
+    
