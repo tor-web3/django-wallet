742c4511fe34ea7ba0e0c42c44bd867cbc449a59
nickname <410058486@qq.com>
2022-06-08 19:59:02 +0800

    代码调优

--- wallet/chainstate/tasks.py
+++ wallet/chainstate/tasks.py
@@ -17,50 +17,32 @@ from test_app.celery import app
 def check_address_status(chain:str=None):
     updated_count = 0
     if chain is None or chain.upper() == 'ETH':
-        updated_count = check_eth_address_status()
+        updated_count = check_all_address_status('ETH')
     if chain is None or chain.upper() == 'TRX':
-        updated_count = updated_count + check_trx_address_status()
+        updated_count = updated_count + check_all_address_status('TRX')
 
     return updated_count
 
-def check_eth_address_status():
-    """
-    检测以太坊地址的余额状态
-    """
-    token_objs = Token.objects.filter(chain__chain_symbol="ETH")
+
+def get_eth_address_balance(state_obj:'State', token_obj:'Token'):
+    addr_obj = state_obj.address
     
-    update_count = 0
-    for token_obj in token_objs:
-        state_objs = State.objects.filter(
-            rpc__chain=token_obj.chain,is_update=False,
-        )[:20]
-        for state_obj in state_objs:
-            addr_obj = state_obj.address
+    response = requests.post(
+        state_obj.rpc.endpoint,
+        json=request_token_balance(
+            addr_obj.address,
+            token_obj.contract_address
+        )
+    )
             
-            response = requests.post(
-                state_obj.rpc.endpoint,
-                json=request_token_balance(
-                    addr_obj.address,
-                    token_obj.contract_address
-                )
-            )
-                    
-            parsed = parse(response.json())
-            if isinstance(parsed, Ok):
-                value:Decimal = Decimal(int(parsed.result,16)) / (10 ** 6)
-                state_obj.balance=value
-                if state_obj.is_update:
-                    update_count = update_count+ 1
-            else:
-                logger.error(parsed.message)
-                
-            state_obj.flush()
+    parsed = parse(response.json())
+    if isinstance(parsed, Ok):
+        value:Decimal = Decimal(int(parsed.result,16)) / (10 ** 6)
+        return value
+    return None
 
-    return update_count
-def get_eth_address_balance(addr_obj:'Address', token_obj:'Token'):
-    pass
-
-def get_trx_address_balance(addr_obj:'Address', token_obj:'Token'):
+def get_trx_address_balance(state_obj:'State', token_obj:'Token'):
+    addr_obj = state_obj.address
     # 构建地址代币余额请求接口
     host_url = "https://apilist.tronscan.org/api/account/tokens"
     token_info_url = f"{host_url}?address={addr_obj.address}&token={token_obj.token_symbol}"
@@ -79,11 +61,15 @@ def get_trx_address_balance(addr_obj:'Address', token_obj:'Token'):
             return value
     return None
 
-# 特定代币的交易记录：https://apilist.tronscan.org/api/token_trc20/transfers?limit=20&start=0&sort=-timestamp&count=true&tokens=TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t&relatedAddress=TD2jixXEuoFTc8LZ26TpN9eJYiG75Staaa
-def check_trx_address_status():
+
+def check_all_address_status(chain = 'TRX'):
     """
     检测波场地址的余额状态
     """
+    get_address_balance = get_eth_address_balance
+    if 'TRX' == chain.upper():
+        get_address_balance = get_trx_address_balance
+
     # 获取波场链下所有代币
     token_objs = Token.objects.filter(chain__chain_symbol="TRX")
 
@@ -94,10 +80,9 @@ def check_trx_address_status():
             rpc__chain=token_obj.chain,is_update=False,
         )[:20]
         for state_obj in state_objs:
-            addr_obj = state_obj.address
 
-            state_obj.balance = get_trx_address_balance(
-                addr_obj=addr_obj,
+            state_obj.balance = get_address_balance(
+                state_obj=state_obj,
                 token_obj=token_obj
             )
     
