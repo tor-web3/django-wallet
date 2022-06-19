dcd6dad21ae7a28e7f685184e7a6c53b11aff100
nickname <410058486@qq.com>
2022-06-19 18:40:17 +0800

    增加提币订单异常的人工审核

--- wallet/history/admin.py
+++ wallet/history/admin.py
@@ -27,17 +27,33 @@ class WithdrawAdmin(admin.ModelAdmin):
     change_form_template = "admin/wallet/history/audit_withdraw.html"
 
     def response_change(self, request, obj):
-        if "allow" in request.POST:
-            matching_names_except_this = self.get_queryset(request).get(pk=obj.id)
-            if matching_names_except_this.status != constant.SUBMITTED:
-                self.message_user(request, _("The order is abnormal and cannot be reviewed"),messages.ERROR)
-                return HttpResponseRedirect(".")
+        if "audit" not in request.POST \
+            and "withdrawing" not in request.POST \
+            and "refuse" not in request.POST:
+            return super().response_change(request, obj)
 
+        matching_names_except_this = self.get_queryset(request).get(pk=obj.id)
+        status = matching_names_except_this.status
+
+        # 审核提币
+        if "audit" in request.POST and status == constant.SUBMITTED:
             matching_names_except_this.status = constant.AUDITED
-            matching_names_except_this.save(update_fields=["status"])
-            self.message_user(request, _("Allow Withdraw"),messages.SUCCESS)
-            return HttpResponseRedirect("..")
-        return super().response_change(request, obj)
+        # 提币遇到错误,人工提币
+        elif "withdrawing" in request.POST and status == constant.ERROR:
+            matching_names_except_this.status = constant.WITHDRAWING
+        # 人工审核拒绝提币
+        elif "refuse" in request.POST and \
+            (status == constant.SUBMITTED or status == constant.ERROR):
+            matching_names_except_this.status = constant.REJECT
+        else :
+        # 非预期的处理类型
+            self.message_user(request, _("The order is abnormal and cannot be reviewed"),messages.ERROR)
+            return HttpResponseRedirect(".")
+
+        matching_names_except_this.save(update_fields=["status"])
+        self.message_user(request, _("Success"),messages.SUCCESS)
+        return HttpResponseRedirect("..")
+
 
 
 admin.site.register(Deposit,DepositAdmin)
