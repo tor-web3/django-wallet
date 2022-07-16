f7d2b55a240a4ca4c5e4224e4a2735a429a9cc79
nickname <410058486@qq.com>
2022-07-16 23:50:25 +0800

    增加地址状态激活API入口

--- wallet/history/views.py
+++ wallet/history/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
 
-# Create your views here.
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+
+class AddressActiveView(View):
+
+    def get(self, request, address, *args, **kwargs):
+        try:
+            from wallet.chainstate.models import State
+            state_obj = State.objects.get(
+                    address__address   = address
+                )
+            state_obj.is_update = True
+            state_obj.flush()
+                
+            return JsonResponse({
+                "results":"success"
+            })
+        except State.DoesNotExist as e:
+            return JsonResponse({
+                "results": e.args
+            })
